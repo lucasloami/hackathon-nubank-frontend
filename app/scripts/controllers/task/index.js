087abeb42f79ceaa328949701b28434356809a31
Lucas <loamilucas@gmail.com>
2016-08-28 09:01:38 -0300

    deployment

--- app/scripts/controllers/task/index.js
+++ app/scripts/controllers/task/index.js
@@ -11,11 +11,11 @@ angular.module('frontendApp')
     .controller('TaskIndexCtrl', function ($scope, $uibModal, $routeParams, Task, Mission) {
       $scope.mission = Mission.get({id: $routeParams.mission_id, user_id: $routeParams.user_id });
 
-      $scope.showTask = function (task_id) {
-        $scope.task = Task.get({id: task_id});
+      $scope.showTask = function (task) {
+        $scope.task = task;
         var modalInstance = $uibModal.open({
           animation: false,
-          component: 'activityModal',
+          component: 'taskModal',
           resolve: {
             task: function () {
               return $scope.task;
@@ -23,8 +23,14 @@ angular.module('frontendApp')
           }
         });
 
-        modalInstance.result.then(function () {
-          // console.log('aqui');
+        modalInstance.result.then(function (resp) {
+          console.log('aqui');
+          var task = resp;
+          task.completed = true;
+          task = new Task(task);
+          var promise = task.$update({mission_id: resp.mission_id});
+          // console.log(promise);
+          // console.log(resp);
         }, function () {
           console.log('ali');
         });
